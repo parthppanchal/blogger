c8fedd92570d87c05c1a062d485cbb2c1f85bd8c
Parth Panchal <parthppanchal@ymail.com>
2015-06-14 12:52:53 +0530

    Adds post metadata

--- generator.py
+++ generator.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template
 from werkzeug import cached_property
 import markdown
+import yaml
 
 POSTS_FILE_EXTENSION = '.md'
 
@@ -11,12 +12,22 @@ app = Flask(__name__)
 class Post(object):
     def __init__(self, path):
         self.path = path
+        self._initialize_metadata()
 
     @cached_property
     def html(self):
         with open(self.path, 'r') as fin:
-            content = fin.read().strip()
+            content = fin.read().split('\n\n', 1)[1].strip()
         return markdown.markdown(content)
+
+    def _initialize_metadata(self):
+        with open(self.path, 'r') as fin:
+            content = ''
+            for line in fin:
+                if not line.strip():
+                    break
+                content += line
+        self.__dict__.update(yaml.load(content))
 
 @app.route('/')
 def index():
--- posts/hello.md
+++ posts/hello.md
@@ -1,3 +1,6 @@
+title: Hello, World!
+date: 2015-06-14
+
 # Hello World
 
 ### This is an H3 level header
